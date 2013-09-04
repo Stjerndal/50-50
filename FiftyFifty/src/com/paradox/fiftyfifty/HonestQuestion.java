d4aad4238f3ff3bd8a8eac31528b602fa605fd66
Måns Odstam <modstam@kth.se>
2013-09-04 13:26:28 +0200

    Formated the layout of DBHandler and HonestQuestion

--- FiftyFifty/src/com/paradox/fiftyfifty/HonestQuestion.java
+++ FiftyFifty/src/com/paradox/fiftyfifty/HonestQuestion.java
@@ -1,39 +1,43 @@
 package com.paradox.fiftyfifty;
 
 /**
- * This class is meant to provide methods used in the "Honest Question" game mode
+ * This class is meant to provide methods used in the "Honest Question" game
+ * mode
+ * 
  * @author Måns Odstam, Andreas Stjerndal, Kalle Sederblad
- *
+ * 
  */
 public class HonestQuestion {
-	
+
 	/**
 	 * constructor
 	 */
-	public HonestQuestion(){
-		//TODO
+	public HonestQuestion() {
+		// TODO
 	}
-	
+
 	/**
 	 * contacts a DBHandler to get a random question from the database
+	 * 
 	 * @return question
 	 */
-	public Question fetchQuestion(){
+	public Question fetchQuestion() {
 		Question question = DBHandler.getRandomQuestion();
 		return question;
 	}
-	
+
 	/**
-	 * Takes a question object and an answer in string form and sees what answer has been given. 
-	 * Then updates the questions statistics in the database.
+	 * Takes a question object and an answer in string form and sees what answer
+	 * has been given. Then updates the questions statistics in the database.
+	 * 
 	 * @param question
 	 * @param answer
 	 */
-	public void answerQuestion(Question question, String answer){
-		if(answer.equals(question.answer1)){
+	public void answerQuestion(Question question, String answer) {
+		if (answer.equals(question.answer1)) {
 			question.setChosenAnswer(1);
 			DBHandler.updateQuestion(question);
-		}else if(answer.equals(question.answer2)){
+		} else if (answer.equals(question.answer2)) {
 			question.setChosenAnswer(2);
 			DBHandler.updateQuestion(question);
 		}
