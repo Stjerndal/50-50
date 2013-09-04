6adf884a436164d7114323dbd12bc1dda1c9c0d9
Måns Odstam <modstam@kth.se>
2013-09-04 12:33:30 +0200

    Created all the classes

---
+++ FiftyFifty/src/com/paradox/fiftyfifty/OneVsOne.java
@@ -0,0 +1,13 @@
+package com.paradox.fiftyfifty;
+
+public class OneVsOne {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
