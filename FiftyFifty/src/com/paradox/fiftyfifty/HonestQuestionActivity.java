a6352300be9b78934c5d616ef2dfd23a32571dcd
Måns Odstam <modstam@kth.se>
2013-09-10 16:19:47 +0200

    Commiting some menu changes

--- FiftyFifty/src/com/paradox/fiftyfifty/HonestQuestionActivity.java
+++ FiftyFifty/src/com/paradox/fiftyfifty/HonestQuestionActivity.java
@@ -2,7 +2,11 @@ package com.paradox.fiftyfifty;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class HonestQuestionActivity extends Activity {
 
@@ -18,5 +22,17 @@ public class HonestQuestionActivity extends Activity {
 		getMenuInflater().inflate(R.menu.honest_question, menu);
 		return true;
 	}
+	
+	 /**
+	  * Called when an answer button is pushed
+	  */
+	 public void buttonPressed(View view){
+	//  Intent intent = new Intent(this, HonestQuestionActivity.class);
+	//  startActivity(intent);
+	  Button button = (Button) view;
+	  TextView textView1 = (TextView) findViewById(R.id.feedback);
+	     textView1.setText("You selected " + button.getText()); 
+	     Intent intent = new Intent(this, MainMenuActivity.class);
+	 }
 
 }
--- FiftyFifty/src/com/paradox/fiftyfifty/MainMenuActivity.java
+++ FiftyFifty/src/com/paradox/fiftyfifty/MainMenuActivity.java
@@ -2,7 +2,9 @@ package com.paradox.fiftyfifty;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class MainMenuActivity extends Activity {
 
@@ -17,6 +19,11 @@ public class MainMenuActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main_menu, menu);
 		return true;
+	}
+	
+	public void submit(View view){
+		Intent intent = new Intent(this, SubmitQuestionActivity.class);
+		startActivity(intent);
 	}
 
 }
