081a72967630d109a14e0584aedd16fd8640b0f5
Måns Odstam <modstam@kth.se>
2013-09-10 13:51:38 +0200

    Pushing string value (activity_submit_question_explanation)

--- FiftyFifty/gen/com/paradox/fiftyfifty/R.java
+++ FiftyFifty/gen/com/paradox/fiftyfifty/R.java
@@ -24,13 +24,9 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080006;
-        public static final int guess_popular=0x7f080001;
-        public static final int honest_button=0x7f080000;
-        public static final int leaderboards=0x7f080003;
-        public static final int my_profile=0x7f080004;
-        public static final int one_vs_one=0x7f080005;
-        public static final int submit_question=0x7f080002;
+        public static final int action_settings=0x7f080002;
+        public static final int email_login_button=0x7f080001;
+        public static final int facebook_login_button=0x7f080000;
     }
     public static final class layout {
         public static final int activity_initial_login_menu=0x7f030000;
@@ -46,18 +42,23 @@ public final class R {
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
+        public static final int activity_submit_question_explanation=0x7f050011;
         public static final int app_name=0x7f050000;
-        public static final int guess_popular=0x7f050007;
-        public static final int hello_world=0x7f050005;
-        public static final int honest_button=0x7f050006;
-        public static final int leaderboards=0x7f050009;
-        public static final int my_profile=0x7f05000a;
-        public static final int one_vs_one=0x7f05000b;
-        public static final int submit_question=0x7f050008;
-        public static final int title_activity_initial_login_menu=0x7f050003;
-        public static final int title_activity_main_menu=0x7f050004;
-        public static final int title_activity_submit_question=0x7f05000c;
-        public static final int tw_facebook_login=0x7f050002;
+        public static final int btn_email_login=0x7f050006;
+        public static final int guess_popular=0x7f05000b;
+        public static final int hello_world=0x7f050009;
+        public static final int honest_button=0x7f05000a;
+        public static final int leaderboards=0x7f05000d;
+        public static final int my_profile=0x7f05000e;
+        public static final int one_vs_one=0x7f05000f;
+        public static final int submit_question=0x7f05000c;
+        public static final int title_activity_initial_login_menu=0x7f050007;
+        public static final int title_activity_main_menu=0x7f050008;
+        public static final int title_activity_submit_question=0x7f050010;
+        public static final int tv_email_login=0x7f050004;
+        public static final int tv_email_login_details=0x7f050005;
+        public static final int tv_facebook_login=0x7f050002;
+        public static final int tv_facebook_login_details=0x7f050003;
     }
     public static final class style {
         /** 
