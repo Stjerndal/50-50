0bb41ab02beff05404452bad5daa54437059566c
Måns Odstam <modstam@kth.se>
2013-09-10 13:04:07 +0200

    test

--- FiftyFifty/gen/com/paradox/fiftyfifty/R.java
+++ FiftyFifty/gen/com/paradox/fiftyfifty/R.java
@@ -36,11 +36,13 @@ public final class R {
         public static final int activity_initial_login_menu=0x7f030000;
         public static final int activity_main=0x7f030001;
         public static final int activity_main_menu=0x7f030002;
+        public static final int activity_submit_question=0x7f030003;
     }
     public static final class menu {
         public static final int initial_login_menu=0x7f070000;
         public static final int main=0x7f070001;
         public static final int main_menu=0x7f070002;
+        public static final int submit_question=0x7f070003;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -54,6 +56,7 @@ public final class R {
         public static final int submit_question=0x7f050008;
         public static final int title_activity_initial_login_menu=0x7f050003;
         public static final int title_activity_main_menu=0x7f050004;
+        public static final int title_activity_submit_question=0x7f05000c;
         public static final int tw_facebook_login=0x7f050002;
     }
     public static final class style {
