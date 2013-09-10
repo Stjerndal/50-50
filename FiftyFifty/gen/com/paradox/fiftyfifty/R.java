d556da8103f6e0ec5f5d9eefaad6ea7039da9225
Måns Odstam <modstam@kth.se>
2013-09-10 12:46:40 +0200

    Switching to testbranch

--- FiftyFifty/gen/com/paradox/fiftyfifty/R.java
+++ FiftyFifty/gen/com/paradox/fiftyfifty/R.java
@@ -24,9 +24,7 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080008;
-        public static final int editText1=0x7f080006;
-        public static final int editText2=0x7f080007;
+        public static final int action_settings=0x7f080006;
         public static final int guess_popular=0x7f080001;
         public static final int honest_button=0x7f080000;
         public static final int leaderboards=0x7f080003;
@@ -38,13 +36,11 @@ public final class R {
         public static final int activity_initial_login_menu=0x7f030000;
         public static final int activity_main=0x7f030001;
         public static final int activity_main_menu=0x7f030002;
-        public static final int activity_submit_question=0x7f030003;
     }
     public static final class menu {
         public static final int initial_login_menu=0x7f070000;
         public static final int main=0x7f070001;
         public static final int main_menu=0x7f070002;
-        public static final int submit_question=0x7f070003;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
