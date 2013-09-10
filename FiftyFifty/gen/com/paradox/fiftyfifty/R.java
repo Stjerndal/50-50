2d4b1bdea4ac633f688f7273a34703e2c9f92112
Måns Odstam <modstam@kth.se>
2013-09-10 12:02:41 +0200

    Created submit question activity

--- FiftyFifty/gen/com/paradox/fiftyfifty/R.java
+++ FiftyFifty/gen/com/paradox/fiftyfifty/R.java
@@ -24,18 +24,27 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080002;
+        public static final int editText1=0x7f080000;
+        public static final int editText2=0x7f080001;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_initial_login_menu=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int activity_submit_question=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int initial_login_menu=0x7f070000;
+        public static final int main=0x7f070001;
+        public static final int submit_question=0x7f070002;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int hello_world=0x7f050005;
+        public static final int title_activity_initial_login_menu=0x7f050003;
+        public static final int title_activity_submit_question=0x7f050004;
+        public static final int tw_facebook_login=0x7f050002;
     }
     public static final class style {
         /** 
